dae6b8db352e40edd1e46f53aa8557ef7980dad8
iconad <amir@icon-ad.com>
2022-09-14 18:33:06 +0400

    mega menu added

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -71,6 +71,29 @@ const joinTheMovementThree = document.querySelector('.join-the-movement-three')
 const joinTheMovementInfo = document.querySelector('.join-the-movement-info')
 const changeTheColorToBlack = document.querySelector('.change-the-color-to-black')
 
+const MegaMenu = document.querySelector('.mega-menu')
+const CloseMegaMenu = document.querySelector('#closeMegaMenu')
+const MegaLi = document.querySelectorAll('.mm-li')
+
+
+let mmtl = gsap.timeline();
+
+gsap.set(MegaMenu, {opacity: 0, zIndex: -50})
+
+mainNavbarBurger.addEventListener('click', function() {
+
+  mmtl.play()
+
+  mmtl.fromTo(MegaMenu, 0.4, {opacity: 0, zIndex: -50}, {opacity: 1, zIndex: 50}, 'one')
+      .fromTo(MegaLi, 0.2, { y: -50, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.1 }, 'one')
+})
+
+CloseMegaMenu.addEventListener('click', function () {
+  mmtl.reverse()
+})
+
+
+
 
   let matchMedia = gsap.matchMedia()
 
